bec42e152b6b876c7d6d1ba376877bdbf9b41399
Sandesh Salunkhe <56861175+Sandesh-Salunkhe@users.noreply.github.com>
2023-12-12 17:08:22 +0530

    Update views.py

--- vendors/Metrics/views.py
+++ vendors/Metrics/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
-from .serializers import *
-from .models import *
-from rest_framework.response import Response, JsonResponse
+from django.http import JsonResponse
 from rest_framework.views import APIView
- 
-# Create your views here.
+from .serializers import VendorSerializer
+from .models import Vendor
 
 class Vendor(APIView):
     serializer_class = VendorSerializer
@@ -14,47 +11,42 @@ class Vendor(APIView):
         return vendor_details
     
     def get(self, request, vendor_id:int = None):
-        if vendor_id != None:
-           if self.get_object(vendor_code):
-              vendor = self.get_vendor(vendor_id)
-              serializer = VendorSerializer(vendor)
-              return JsonResponse({'Success': True, 'data': serializer.data})
-            return JsonResponse({'Success': False, 'data': serializer.data})
-
+        if vendor_id is not None:
+            if Vendor.objects.filter(id=vendor_id).exists():
+                vendor = self.get_vendor(vendor_id)
+                serializer = VendorSerializer(vendor)
+                return JsonResponse({'Success': True, 'data': serializer.data})
+            return JsonResponse({'Success': False, 'Message': 'Vendor not found'})
+        
         all_vendors = Vendor.objects.all()
-        vendors_data = VendorSerializer(all_vendors,many=True)
+        vendors_data = VendorSerializer(all_vendors, many=True)
         return JsonResponse({'Success': True, 'data': vendors_data.data})
     
     def post(self, request):
-        
         vendor_name = request.POST.get('vendor_name')
         contact_details = request.POST.get('contact_details')
         address = request.POST.get('address')
         
-        vendor_creation = Vendor.objeect.create(name = vendor_name, contact_details = contact_details, address = address)
+        vendor_creation = Vendor.objects.create(name=vendor_name, contact_details=contact_details, address=address)
         
-        
-        return JsonResponse({'Success': True, 'Message':'Vendor Profile Created Successfully.'})
+        return JsonResponse({'Success': True, 'Message': 'Vendor Profile Created Successfully.'})
     
-    def put(self,request,vendor_id:int):
-        
+    def put(self, request, vendor_id:int):
         name = request.POST.get('name')
         contact_details = request.POST.get('contact_details')
         address = request.POST.get('address')
 
-        Vendor.objects.get(id = vendor_id).update(
-            name = name,
-            contact_details = contact_details,
-            address = address
-        )
-     
-        return JsonResponse({'Success': True, 'Message':'Vendor Profile Created Successfully.'})
-    
-    def delete(self,vendor_id:int):
-        remove_vendor = Vendor.objects.get(id= vendor_id)
-        if remove_vendor:
-           remove_vendor.delete()
-           return JsonResponse({'Success': True, 'Message':'Vendor Profile Deleted Successfully.'})
-        return JsonResponse({'Success': False, 'Message':'Vendor Profile Not Found.'})
+        vendor = Vendor.objects.get(id=vendor_id)
+        vendor.name = name
+        vendor.contact_details = contact_details
+        vendor.address = address
+        vendor.save()
 
-       
+        return JsonResponse({'Success': True, 'Message': 'Vendor Profile Updated Successfully.'})
+    
+    def delete(self, request, vendor_id:int):
+        remove_vendor = Vendor.objects.filter(id=vendor_id).first()
+        if remove_vendor:
+            remove_vendor.delete()
+            return JsonResponse({'Success': True, 'Message': 'Vendor Profile Deleted Successfully.'})
+        return JsonResponse({'Success': False, 'Message': 'Vendor Profile Not Found.'})
