207d4c1b284a29961257939d69e7c7bbc6b7118e
backuser3 <backuser3@qr4order.com>
2023-12-19 11:55:39 +0530

    updated code

--- vendors/Metrics/urls.py
+++ vendors/Metrics/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from .views import *
 urlpatterns = [
-    # path('',)
+    path('api/vendors/', VendorList.as_view(), name='vendor-list'),
+    path('api/vendors/<int:vendor_id>/', VendorDetail.as_view(), name='vendor-detail'),
 ]
--- vendors/Metrics/views.py
+++ vendors/Metrics/views.py
@@ -1,40 +1,41 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Response
 from rest_framework.views import APIView
 from .serializers import VendorSerializer
 from .models import Vendor
+from django.db import connection
 
-class Vendor(APIView):
+ 
+class VendorList(APIView):
     serializer_class = VendorSerializer
     
-    def get_vendor(self, vendor_id):
-        vendor_details = Vendor.objects.filter(id=vendor_id).first()
-        return vendor_details
-    
-    def get(self, request, vendor_id:int = None):
-        if vendor_id is not None:
-            if Vendor.objects.filter(id=vendor_id).exists():
-                vendor = self.get_vendor(vendor_id)
-                serializer = VendorSerializer(vendor)
-                return JsonResponse({'Success': True, 'data': serializer.data})
-            return JsonResponse({'Success': False, 'Message': 'Vendor not found'})
-        
+    def get(self, request):
         all_vendors = Vendor.objects.all()
         vendors_data = VendorSerializer(all_vendors, many=True)
         return JsonResponse({'Success': True, 'data': vendors_data.data})
-    
+
     def post(self, request):
-        vendor_name = request.POST.get('vendor_name')
-        contact_details = request.POST.get('contact_details')
-        address = request.POST.get('address')
+        vendor_name = request.data.get('vendor_name')
+        contact_details = request.data.get('contact_details')
+        address = request.data.get('address')
         
         vendor_creation = Vendor.objects.create(name=vendor_name, contact_details=contact_details, address=address)
         
         return JsonResponse({'Success': True, 'Message': 'Vendor Profile Created Successfully.'})
+
+class VendorDetail(APIView):
+    serializer_class = VendorSerializer
     
+    def get(self, request, vendor_id:int):
+        vendor = Vendor.objects.filter(id=vendor_id).first()
+        if vendor is not None:
+            serializer = VendorSerializer(vendor)
+            return JsonResponse({'Success': True, 'data': serializer.data})
+        return JsonResponse({'Success': False, 'Message': 'Vendor not found'})
+
     def put(self, request, vendor_id:int):
-        name = request.POST.get('name')
-        contact_details = request.POST.get('contact_details')
-        address = request.POST.get('address')
+        name = request.data.get('name')
+        contact_details = request.data.get('contact_details')
+        address = request.data.get('address')
 
         vendor = Vendor.objects.get(id=vendor_id)
         vendor.name = name
@@ -50,3 +51,12 @@ class Vendor(APIView):
             remove_vendor.delete()
             return JsonResponse({'Success': True, 'Message': 'Vendor Profile Deleted Successfully.'})
         return JsonResponse({'Success': False, 'Message': 'Vendor Profile Not Found.'})
+
+
+
+class PurchaseOrder(APIView):
+
+    def get(self, request):
+
+        return Response({"Success": True, 'Message': "Purchase Order"})
+
